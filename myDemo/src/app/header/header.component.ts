265545f9cc21194b213a75809dfa27564eac2695
Vanessa Militello <militello.vane@gmail.com>
2018-03-14 10:49:22 +0100

    creazione header

---
+++ myDemo/src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  id: number;
+  titolo: string;
+  conferma: boolean;
+
+  constructor() {
+    this.id = 1;
+    this.titolo = 'ciao';
+    this.conferma = true;
+  }
+
+  ngOnInit() {
+  }
+
+}
